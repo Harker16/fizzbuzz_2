1fa67d613dd02123b3d02c6a80d52786efbbd879
Thomas Harker <tom.harker@hotmail.co.uk>
2016-06-21 13:17:25 +0100

    Add completed fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,49 +1,68 @@
 describe('FizzBuzz', function() {
 
+		var fizzbuzz;
+
 	beforeEach(function(){
-		fizzbuzz = new FizzBuzz
+		fizzbuzz = new FizzBuzz();
 	});
 
 	describe('number checker', function() {
 		it('checks if number is divisibe by 3', function() {
-			expect(fizzbuzz.isDivisibleByThree(3)).toBe(true)
+			expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
 		});
 	});
 
 		describe('number checker', function() {
 		it('checks if number is not divisibe by 3', function() {
-			expect(fizzbuzz.isDivisibleByThree(4)).toBe(false)
+			expect(fizzbuzz.isDivisibleByThree(4)).toBe(false);
 		});
 	});
 
 		describe('number checker', function() {
 		it('checks if number is divisibe by 5', function() {
-			expect(fizzbuzz.isDivisibleByFive(5)).toBe(true)
+			expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
 		});
 	});
 
 		describe('number checker', function() {
 		it('checks if number is not divisibe by 5', function() {
-			expect(fizzbuzz.isDivisibleByFive(4)).toBe(false)
+			expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
 		});
 	});
 
 	describe('number checker', function() {
 		it('checks if number is divisibe by 15', function() {
-			expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true)
+			expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
 		});
 	});
 
 		describe('number checker', function() {
 		it('checks if number is not divisibe by 15', function() {
-			expect(fizzbuzz.isDivisibleByFifteen(4)).toBe(false)
+			expect(fizzbuzz.isDivisibleByFifteen(4)).toBe(false);
 		});
 	});
 
 		describe('fizzer', function() {
 			it('returns fizz if number is divisible by 3', function() {
-				expect(fizzbuzz.fizzer(3)).toEqual('fizz')
+				expect(fizzbuzz.fizzer(3)).toEqual('fizz');
 			});
 		});
 
+		describe('fizzer', function() {
+			it('returns buzz if number is divisible by five', function() {
+					expect(fizzbuzz.fizzer(5)).toEqual('buzz');
+			});
+		});
+
+		describe('fizzer', function() {
+			it('returns fizzbuzz if number is divisible by fifteen', function() {
+					expect(fizzbuzz.fizzer(15)).toEqual('fizzbuzz');
+			});
+		});
+
+		describe('fizzer', function() {
+			it('returns number if number is not divisible by three or five', function() {
+					expect(fizzbuzz.fizzer(4)).toEqual(4);
+			});
+		});
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,20 +1,27 @@
 function FizzBuzz() {};
 
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
-	return (number % 3 === 0)
+	return (number % 3 === 0);
 };
 
 FizzBuzz.prototype.isDivisibleByFive = function(number) {
-	return (number % 5 === 0)
+	return (number % 5 === 0);
 };
 
 FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
-	return (number % 15 === 0)
+	return (number % 15 === 0);
 };
 
 FizzBuzz.prototype.fizzer = function(number) {
-	if(this.isDivisibleByThree(number)){
-		return 'fizz'
+	if(this.isDivisibleByFifteen(number)){
+		return 'fizzbuzz';
 	}
+	if(this.isDivisibleByThree(number)){
+		return 'fizz';
+	}
+	if(this.isDivisibleByFive(number)) {
+		return 'buzz';
+	}
+	return number;
 };
 
